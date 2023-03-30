60a1d1b1fbfb8d236da03a023fbf0bcb792a00f4
bl4ko <go3969@student.uni-lj.si>
2023-03-30 19:41:07 +0200

    Refractor, add comments, fix duplicates of map_manager.py

--- src/main/task1/scripts/map_manager.py
+++ src/main/task1/scripts/map_manager.py
@@ -1,197 +1,177 @@
 #!/usr/bin/python3
-
-import math
+import numpy as np
+import rospy
 import threading
 import cv2
+import tf2_geometry_msgs
 from matplotlib import pyplot as plt
-import numpy as np
-import yaml
-import rospy
-import move_base
-import rospkg
 from nav_msgs.msg import OccupancyGrid
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Pose
-from actionlib_msgs.msg import GoalStatus
-from skimage.morphology import skeletonize  # Import Pose
-from geometry_msgs.msg import TransformStamped
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point
-import sys
-
+from skimage.morphology import skeletonize
 from std_msgs.msg import ColorRGBA
-from geometry_msgs.msg import PointStamped, Vector3, Pose,Quaternion
-
+from geometry_msgs.msg import PointStamped, Vector3, Point, Quaternion, TransformStamped
 from visualization_msgs.msg import Marker, MarkerArray
 from tf2_geometry_msgs import PointStamped
-from tf2_ros import TransformListener
-from tf2_geometry_msgs import PointStamped
-from tf2_ros import Buffer, TransformListener
-import tf2_ros
-import tf2_geometry_msgs
-
-import rospy
-from actionlib import SimpleActionClient
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Pose, Point, Quaternion
-
-from map_msgs.msg import OccupancyGridUpdate
+from typing import Tuple
 
 
-
-#this file gets the map from the map_server and saves it in variable map
-#it also uses skeletonize to get points to visit in the map
-#it also publishes the points to visit as markers in rviz
+# This script retrieves the map from the map_server and saves it in a 2D array.
+# It also uses ekeletonize to get most important points to visit in the map.
+# The points to visit are published as markers in rviz.
 
 
 class MapManager:
+    """
+    A class for managaing a map, processing it and publishing markers for points to visit.
+    """
+
     def __init__(self):
         self.map = None
-        self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
+        self.skeleton_overlay = None
+        self.branch_points = None
+        self.map_subscriber = rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
         self.map_lock = threading.Lock()  # Add a lock for the map attribute
-
-        self.marker_pub = rospy.Publisher("MapManager_markers", MarkerArray, queue_size=100)
-        
+        self.marker_publisher = rospy.Publisher(
+            "goal_markers", MarkerArray, queue_size=100
+        )
         self.goals_ready = False
-        self.goals = []
-
+        self.goal_points = []
         self.map_transform = TransformStamped()
         self.map_resolution = None
-
         self.size_x = None
         self.size_y = None
-        self.map_reference_frame = None
+        self.map_frame_id = None
 
-
-
-
-    
-    def map_callback(self, data):
-         with self.map_lock:  # Acquire the lock before modifying the map attribute
+    def map_callback(self, data) -> None:
+        """
+        Callback function for the map subscriber. It processes the map data when received.
+        """
+        with self.map_lock:  # Acquire the lock before modifying the map attribute
             self.map_processing(data)
 
+    def get_map(self) -> np.ndarray:
+        """
+        Get the current map.
 
-    def get_map(self):
+        Returns:
+            numpy.ndarray: The current map
+        """
         with self.map_lock:  # Acquire the lock before accessing the map attribute
             return self.map
-        
+
     def is_ready(self):
+        """
+        Check if the goals are ready.
+
+        Returns:
+            bool: True if the goals are ready, False otherwise.
+        """
         with self.map_lock:
             return self.goals_ready
-    
 
     def get_goals(self):
+        """Get the list of goals
+
+        Returns:
+            list: List of goals.
+        """
         with self.map_lock:
-            return self.goals
-        
-    
+            return self.goal_points
 
+    def map_processing(self, map_data):
+        """
+        Process the map data to find goals and publish markers
 
-    def map_processing(self,map_data):
+        Args:
+            map_data (OccupancyGrid): Map data to process.
+        """
 
-        size_x = map_data.info.width
-        size_y = map_data.info.height
+        self.size_x = map_data.info.width
+        self.size_y = map_data.info.height
 
-        self.size_x = size_x
-        self.size_y = size_y
+        rospy.loginfo("Map size: x: %s, y: %s." % (str(self.size_x), str(self.size_y)))
 
-        rospy.loginfo("Map size: x: %s, y: %s." % (str(size_x), str(size_y)))
-
-        if size_x < 3 or size_y < 3:
-            rospy.loginfo("Map size only: x: %s, y: %s. NOT running map to image conversion." % (str(size_x), str(size_y)))
+        if self.size_x < 3 or self.size_y < 3:
+            rospy.loginfo(
+                "Map size only: x: %s, y: %s. NOT running map to image conversion."
+                % (str(self.size_x), str(self.size_y))
+            )
             return
-        
+
+        # Get the map properties from the map topic
         self.map_resolution = map_data.info.resolution
-        rospy.loginfo("Map resolution: %s" % str(self.map_resolution))
-
-        self.map_reference_frame = map_data.header.frame_id
-
+        self.map_frame_id = map_data.header.frame_id
         self.map_transform.transform.translation.x = map_data.info.origin.position.x
         self.map_transform.transform.translation.y = map_data.info.origin.position.y
         self.map_transform.transform.translation.z = map_data.info.origin.position.z
         self.map_transform.transform.rotation = map_data.info.origin.orientation
 
-        # deal with map
-        #self.map = np.array(map_data.data, dtype = np.int8).reshape((size_y, size_x))
-        self.map = np.array(map_data.data).reshape((size_y, size_x))
+        # 2D array representing the map
+        self.map = np.array(map_data.data).reshape((self.size_y, self.size_x))
 
-        # flip on the rows to get correct image (flip along y axis)
-        # is it neccesary in our case - doe not seem to be because we will not transform y later at conversion
-        # self.map = np.flip(self.map, 0)
-
-        # get correct numbers
+        # Assign correct values to the map (0 = free, 100 = occupied, -1 = unknown)
         self.map[self.map == -1] = 127
         self.map[self.map == 0] = 255
         self.map[self.map == 100] = 0
 
-        # convert to uint8
+        # Convert the map to uint8
+        # Remove small obstacles and enlarge free spaces in the map
+        # This makes it safer for the robot to navigate
         self.map = self.map.astype(np.uint8)
-
-        kernel = np.ones((3,3),np.uint8)
-        semi_safe_map = cv2.erode(self.map,kernel,iterations = 3)
-
-        #make it safe to drive on 
+        kernel = np.ones((3, 3), np.uint8)
+        semi_safe_map = cv2.erode(self.map, kernel, iterations=3)
         self.map = semi_safe_map
 
-        #save map as image
-        #cv2.imwrite("map_map_manager.png", self.map)
+        # Find the skeleton overlay of the map
+        self.skeleton_overlay = self.skeletonize_map()
 
-        #magic code start do not touch
-        f=np.flip(self.map,0)
-        skeleton_ = skeletonize(f)
-        #unflip
-        skeleton_ = np.flip(skeleton_,0)   
+        # find branch points note the map is flipped
+        self.branch_points = self.find_branch_points()
 
-        skeleton_image = np.zeros_like(skeleton_, dtype=np.uint8)
-        skeleton_image[skeleton_] = 255
+        # self.visualize_branch_points()
 
+        self.init_goals()
 
-        final = skeleton_image[self.map == 255]
-        final_resized = cv2.resize(final, self.map.shape[::-1], interpolation=cv2.INTER_NEAREST)
-        final = final_resized
-
-        new_image = np.zeros_like(self.map )
-        new_image[self.map  == 255] = skeleton_image[self.map  == 255]
-
-        #magic code stop
-
-        #find branch points note the map is flipped
-        bp = self.find_branch_points(new_image)
-        
-        bp_map = np.zeros(self.map.shape, dtype=np.uint8)
-        
-        for i in range(len(bp)):
-            bp_map[bp[i][1]][bp[i][0]] = 255
-
-        #save branch points as image
-        #cv2.imwrite("branch_points_map_manager.png", bp_map)
-
-        
-        #visualize_map(bp_map, "branch points")
-        
-        #overlay branch points on map
-        #overlayed = cv2.addWeighted(self.map, 0.5, bp_map, 0.5, 0)
-        
-        #visualize_map(overlayed, "overlayed")
-
+    def init_goals(self) -> None:
         goals = []
-        for i in range(len(bp)):
-            goals.append(self.map_to_world_coords(bp[i][0], bp[i][1]))
+        for i in range(len(self.branch_points)):
+            goals.append(
+                self.map_to_world_coords(
+                    self.branch_points[i][0], self.branch_points[i][1]
+                )
+            )
 
-        
-        self.goals = goals
+        self.goal_points = goals
         self.publish_markers_of_goals(goals)
-
         self.goals_ready = True
 
-        
+    def skeletonize_map(self) -> np.ndarray:
+        # Skeletonize the map: The map is flipped vertically, skletenozied
+        # and then flipped back. The skeletonization process finds the "skeleton"
+        # of the free space in the map, which is a thinned, single-pixel wide
+        # version of the original free space
+        flipped_map = np.flip(self.map, 0)
+        skeleton = skeletonize(flipped_map)
+        skeleton = np.flip(skeleton, 0)
 
+        # Create a skeleton image
+        skeleton_image = np.zeros_like(skeleton, dtype=np.uint8)
+        skeleton_image[skeleton] = 255
 
+        # Extract the skeleton from the original map: create a new
+        # image containing only the skeleton pixels where the original map
+        # was free (255)
+        skeleton_overlay = np.zeros_like(self.map)
+        skeleton_overlay[self.map == 255] = skeleton_image[self.map == 255]
 
-        
+        return skeleton_overlay
 
-    
-    def publish_markers_of_goals(self, goals):
+    def publish_markers_of_goals(self, goals: list) -> None:
+        """
+        Publish markers for the goals in RViz
+
+        Args:
+            goals (list): List of goals.
+        """
 
         marker_array = MarkerArray()
 
@@ -200,35 +180,29 @@ class MapManager:
             marker.id = i
             marker.pose.position = Point(goal[0], goal[1], 0)
             marker.header.stamp = rospy.Time(0)
-            marker.header.frame_id = 'map'
+            marker.header.frame_id = "map"
             marker.pose.orientation = Quaternion(0.5, 0.5, 0.5, 0.5)
             marker.type = Marker.CUBE
             marker.action = Marker.ADD
             marker.frame_locked = False
             marker.lifetime = rospy.Duration(2)
             marker.scale = Vector3(0.1, 0.1, 0.1)
-            #orange color
-            marker.color = ColorRGBA(1.0, 0.5, 0.0, 1.0)
-
+            marker.color = ColorRGBA(1.0, 0.5, 0.0, 1.0)  # orange color
             marker_array.markers.append(marker)
-            print("added marker" + str(goal))
 
+        self.marker_publisher.publish(marker_array)
 
-        self.marker_pub.publish(marker_array)
+    def map_to_world_coords(self, x: float, y: float) -> Tuple[float, float]:
+        """
+        Transform map coordinates to world coordinates.
 
+        Args:
+            x (float): The x-coordinate in the map frame.
+            y (float): The y-coordinate in the map frame.
 
-
-        
-        
-
-
-
-
-
-    """
-    Returns coordinates transformed from map to world.
-    """
-    def map_to_world_coords(self, x, y):
+        Returns:
+            Tuple[float, float]: A tuple containing the x and y coordinates in the world frame.
+        """
         pt = PointStamped()
         pt.point.x = x * self.map_resolution
         pt.point.y = y * self.map_resolution
@@ -239,47 +213,78 @@ class MapManager:
 
         return (transformed_pt.point.x, transformed_pt.point.y)
 
+    def find_branch_points(self) -> list:
+        """
+        Find branch points in a given skeleton image.
 
+        Args:
+            skeleton_image (np.ndarray): A binary image containing the skeleton.
 
-        
-    def find_branch_points(self,skeleton_image):
-        sc=skeleton_image.copy()
-        #0-1
-        dst = cv2.cornerHarris(sc, 9, 5, 0.04)
-        # result is dilated for marking the corners
-        dst = cv2.dilate(dst,None)
+        Returns:
+            list: A list of tuples (x, y) containing the branch point coordinates.
+        """
+        # Create a copy of the input image to avoid modifying the original
+        skeleton_copy = self.skeleton_overlay.copy()
 
-        # Threshold for an optimal value, it may vary depending on the image.
-        img_thresh = cv2.threshold(dst, 0.32*dst.max(), 255, 0)[1]
-        img_thresh = np.uint8(img_thresh)
+        # Apply the Harris corner detector to find corners in the image
+        harris_corners = cv2.cornerHarris(skeleton_copy, blockSize=9, ksize=5, k=0.04)
 
-        # get the matrix with the x and y locations of each centroid
-        centroids = cv2.connectedComponentsWithStats(img_thresh)[3]
+        # Dilate the result to make the corners more visible
+        harris_corners_dilated = cv2.dilate(harris_corners, None)
 
+        # Apply thresholding to identify the optimal corners
+        threshold_value = 0.32 * harris_corners_dilated.max()
+        threshold_image = cv2.threshold(
+            harris_corners_dilated, threshold_value, 255, 0
+        )[1]
+        threshold_image_uint8 = np.uint8(threshold_image)
+
+        # Find connected components and their centroids
+        centroids = cv2.connectedComponentsWithStats(threshold_image_uint8)[3]
+
+        # Define criteria for refining corner coordinates
         stop_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # refine corner coordinates to subpixel accuracy
-        corners = cv2.cornerSubPix(sc, np.float32(centroids), (5,5), (-1,-1), stop_criteria)
-        
-        toret=[]
-        for i in range(1, len(corners)):
-            toret.append((int(corners[i,0]), int(corners[i,1])))
-        
-        return toret
+
+        # Refine corner coordinates to subpixel accuracy
+        refined_corners = cv2.cornerSubPix(
+            skeleton_copy, np.float32(centroids), (5, 5), (-1, -1), stop_criteria
+        )
+
+        # Extract the branch point coordinates from the refined corners
+        branch_points = [
+            (int(corner[0]), int(corner[1])) for corner in refined_corners[1:]
+        ]
+
+        return branch_points
+
+    def visualize_branch_points(self) -> None:
+        """
+        Visualize the branch points in the map.
+        """
+        bp_map = np.zeros(self.map.shape, dtype=np.uint8)
+
+        for i in range(len(self.branch_points)):
+            bp_map[self.branch_points[i][1]][self.branch_points[i][0]] = 255
+
+        overlayed_bp = cv2.addWeighted(self.map, 0.5, bp_map, 0.5, 0)
+
+        cv2.imshow("overlayed branch points", overlayed_bp)
+        cv2.waitKey(0)
 
 
 def visualize_map(map_data, title="vis"):
-    plt.imshow(map_data, cmap='gray', origin='lower')
+    plt.imshow(map_data, cmap="gray", origin="lower")
     plt.title(title)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.show()
-        
-        
+
+
 def test():
-    rospy.init_node('path_setter', anonymous=True)
+    rospy.init_node("path_setter", anonymous=True)
     ps = MapManager()
 
-    #run node and wait for map the nshow it in plt
+    # run node and wait for map the nshow it in plt
     rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
         if ps.is_ready():
@@ -291,10 +296,7 @@ def test():
                 ps.publish_markers_of_goals(goals)
             rate.sleep()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("start")
     test()
-
-        
-        
-    
